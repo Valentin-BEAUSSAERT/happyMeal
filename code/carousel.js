49b50083f6752fcb1ac3cb651c98fd4913c9d267
SK4 <valentin.beaussaert@laplateforme.io>
2024-04-08 18:34:38 +0200

    test avec image ia

---
+++ code/carousel.js
@@ -0,0 +1,29 @@
+function setImage(imagePath) {
+    const mainImage = document.getElementById('mainImage');
+    mainImage.src = imagePath;
+  
+    // Ajouter la classe pour l'effet de transition
+    mainImage.classList.add('fade');
+  
+    // Supprimer la classe après que l'animation soit terminée pour permettre à l'effet de se rejouer
+    setTimeout(() => {
+      mainImage.classList.remove('fade');
+    }, 1000); // Correspond à la durée de l'animation
+  }
+  
+  let currentImageIndex = 0;
+  const images = ['../assets/1.jpeg', '../assets/2.jpeg', '../assets/3.jpeg', '../assets/4.jpeg', '../assets/5.webp'];
+  
+  function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    setImage(images[currentImageIndex]);
+  }
+  
+  function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    setImage(images[currentImageIndex]);
+  }
+  
+  // Faire défiler automatiquement les images
+  setInterval(nextImage, 5000);
+  
