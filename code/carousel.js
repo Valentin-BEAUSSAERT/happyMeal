934552dfb2f7d50653e5f8dc7792c583755a36ac
SK4 <valentin.beaussaert@laplateforme.io>
2024-04-09 09:23:24 +0200

    modification script carousel pour image aléatoire

--- code/carousel.js
+++ code/carousel.js
@@ -12,7 +12,7 @@ function setImage(imagePath) {
   }
   
   let currentImageIndex = 0;
-  const images = ["../assets/30.webp", "../assets/31.webp", "../assets/32.webp", "../assets/33.webp", "../assets/34.webp", "../assets/35.webp", "../assets/36.webp", "../assets/37.webp", "../assets/38.webp", "../assets/39.webp", "../assets/40.webp" ];
+  const images = ["../assets/30.webp", "../assets/31.webp", "../assets/32.webp", "../assets/33.webp", "../assets/34.webp", "../assets/35.webp", "../assets/36.webp", "../assets/37.webp", "../assets/38.webp", "../assets/39.webp", "../assets/40.webp"];
   
   function nextImage() {
     currentImageIndex = (currentImageIndex + 1) % images.length;
@@ -24,8 +24,15 @@ function setImage(imagePath) {
     setImage(images[currentImageIndex]);
   }
   
-  // Faire défiler automatiquement les images
-
-
+  // Fonction pour choisir une image aléatoire au chargement
+  function chooseRandomImageOnLoad() {
+    currentImageIndex = Math.floor(Math.random() * images.length); // Choisir un index aléatoire
+    setImage(images[currentImageIndex]); // Mettre à jour l'image principale avec cet index
+  }
+  
+  // Écouteur d'événement pour exécuter la fonction au chargement complet de la page
+  document.addEventListener('DOMContentLoaded', chooseRandomImageOnLoad);
+  
+  // Décommentez la ligne ci-dessous pour faire défiler automatiquement les images
   // setInterval(nextImage, 5000);
   
