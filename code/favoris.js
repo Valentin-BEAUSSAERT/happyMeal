03e071518c2ee7d644bfec53c9589f6b7656e0fe
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-04-10 11:32:15 +0200

    affichage du bouton like dans la modal de la recette

--- code/favoris.js
+++ code/favoris.js
@@ -11,3 +11,20 @@ likeButtons.forEach((button, index) => {
     recipeDiv.remove();
   });
 });
+
+// Récupérer les données de la recette depuis le stockage local
+const recetteJSON = localStorage.getItem("favoris");
+const favorisContainer = document.getElementById("favorisContainer");
+
+// Afficher la recette dans le conteneur des favoris si elle existe
+if (recetteJSON) {
+  const recette = JSON.parse(recetteJSON);
+  favorisContainer.innerHTML = recette.contenu;
+  const likeButton = favorisContainer.querySelector(".like-button");
+  likeButton.style.color = "red";
+  // Utilisez l'index pour afficher l'image correspondante
+  const index = recette.index;
+  // Faites quelque chose avec l'index pour afficher l'image
+} else {
+  favorisContainer.innerHTML = "<p>Aucune recette likée pour le moment.</p>";
+}
